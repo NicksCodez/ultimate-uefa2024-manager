5d09cf15b40cc03ac2d45dc6cb82e0ddd9816a66
NicksCodez <elisei.nicolae08@gmail.com>
2024-07-22 01:34:26 +0300

    fix profile page bug

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 
 // firebase
 import { doc, getDoc } from 'firebase/firestore';
@@ -14,39 +14,64 @@ import { useAuth } from '../../contexts/authContext/AuthContext';
 // css
 import './Profile.css';
 
+// assets
+import DefaultProfile from '../../../assets/defaults/default_profile.png';
+
 const Profile = () => {
   // component state
   const [userData, setUserData] = useState(null);
   const [userDataLoading, setUserDataLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const navigate = useNavigate();
+
   // logged in user
   const { currentUser } = useAuth();
 
+  // back click function
+  const onBackClick = () => {
+    navigate(-1);
+  };
+
   useEffect(() => {
-    getUserData(currentUser.uid)
-      .then((data) => {
-        setUserData(data);
-        setUserDataLoading(false);
-      })
-      .catch((fetchError) =>
-        console.error('error getting user data: ', fetchError),
-      );
+    if (!currentUser.isAnonymous) {
+      getUserData(currentUser.uid)
+        .then((data) => {
+          setUserData(data);
+          setUserDataLoading(false);
+        })
+        .catch((fetchError) =>
+          console.error('error getting user data: ', fetchError),
+        );
+    } else {
+      setUserDataLoading(false);
+    }
   }, []);
 
   return (
     <div id="profile">
       {!currentUser && <Navigate to="/login" />}
-      <PageHeader title="Profile" />
+      <PageHeader title="Profile" onBackClick={onBackClick} />
       {!userDataLoading && (
         <div id="profile-main">
           <div className="profile-card">
             <div className="profile-card-left">
-              <img src={userData.profilePicUrl} alt="user" />
+              <img
+                src={
+                  !currentUser.isAnonymous
+                    ? userData.profilePicUrl
+                    : DefaultProfile
+                }
+                alt="user"
+              />
             </div>
             <div className="profile-card-right">
-              <div className="profile-card-name">{userData.name} </div>
-              <div className="profile-card-username">@{userData.username} </div>
+              <div className="profile-card-name">
+                {!currentUser.isAnonymous ? userData.name : 'Anon'}{' '}
+              </div>
+              <div className="profile-card-username">
+                @{!currentUser.isAnonymous ? userData.username : 'anon'}{' '}
+              </div>
             </div>
           </div>
           <Link to="/logout" className="authentication-button">
