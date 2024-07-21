a1d623a7bc7c86ceb5129551a40abd16317c639a
NicksCodez <elisei.nicolae08@gmail.com>
2024-07-21 20:44:26 +0300

    style login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -15,6 +15,9 @@ import useInputField from '../../utils/hooks/inputHooks/UseInput';
 // context
 import { useAuth } from '../../contexts/authContext/AuthContext';
 
+// css
+import './Login.css';
+
 const Login = () => {
   const { loggedIn } = useAuth();
 
@@ -46,40 +49,42 @@ const Login = () => {
     <div id="login">
       {loggedIn && <Navigate to="/home" replace />}
       <PageHeader title="Login" onBackClick={onBackClick} />
-      <form>
-        <InputField
-          id="emailInput"
-          type="email"
-          name="email"
-          label="Email"
-          value={emailValue}
-          onChange={emailOnChange}
-          onFocus={emailOnFocus}
-          onBlur={emailOnBlur}
-        />
-        <InputField
-          id="passwordInput"
-          type="password"
-          name="password"
-          label="Password"
-          value={passwordValue}
-          onChange={passwordOnChange}
-          onFocus={passwordOnFocus}
-          onBlur={passwordOnBlur}
-        />
-        <button
-          type="submit"
-          onClick={(e) =>
-            handleLogin(e, emailValue, passwordValue, setLoggingIn, navigate)
-          }
-        >
-          Log In
-        </button>
-      </form>
-
-      <p className="no-account">
-        Don&apos;t have an account? <a href="/signup">Sign up!</a>
-      </p>
+      <div className="form-wrapper">
+        <form>
+          <InputField
+            id="emailInput"
+            type="email"
+            name="email"
+            label="Email"
+            value={emailValue}
+            onChange={emailOnChange}
+            onFocus={emailOnFocus}
+            onBlur={emailOnBlur}
+          />
+          <InputField
+            id="passwordInput"
+            type="password"
+            name="password"
+            label="Password"
+            value={passwordValue}
+            onChange={passwordOnChange}
+            onFocus={passwordOnFocus}
+            onBlur={passwordOnBlur}
+          />
+          <button
+            type="submit"
+            onClick={(e) =>
+              handleLogin(e, emailValue, passwordValue, setLoggingIn, navigate)
+            }
+            className="authentication-button"
+          >
+            Log In
+          </button>
+        </form>
+        <p className="no-account">
+          Don&apos;t have an account? <a href="/signup">Sign up!</a>
+        </p>
+      </div>
       {loggingIn && <div>Logging in</div>}
     </div>
   );
